31907cb5327f01e6c138bbed6393c2b79b731c11
coinsccg <59504432+coinsccg@users.noreply.github.com>
2022-04-24 15:25:27 +0800

    Update localShareAndLPShare.sol

--- contracts/localShareAndLPShare.sol
+++ contracts/localShareAndLPShare.sol
@@ -288,7 +288,7 @@ contract ERC20Token is Ownable, IERC20 {
     }
 
     function includeInReward(address account) external onlyOwner {
-        require(_isExcluded[account], "Account is already excluded");
+        require(_isExcluded[account], "Account is already included");
         for (uint256 i = 0; i < _excluded.length; i++) {
             if (_excluded[i] == account) {
                 _excluded[i] = _excluded[_excluded.length - 1];
