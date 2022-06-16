63941a07df585e86c6d243671f88601715f6826e
coinsccg <59504432+coinsccg@users.noreply.github.com>
2022-06-16 14:02:45 +0800

    save gas

--- contracts/string_utils.sol
+++ contracts/string_utils.sol
@@ -697,18 +697,18 @@ contract StringUtils{
     function toAsciiString(address x) public pure returns (string memory) {
         bytes memory s = new bytes(40);
         for (uint i = 0; i < 20; i++) {
-            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
-            bytes1 hi = bytes1(uint8(b) / 16);
-            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            uint8 b = uint8(uint(uint160(x)) / (2**(8*(19 - i))));
+            uint8 hi = b / 16;
+            uint8 lo = b - 16 * hi;
             s[2*i] = char(hi);
             s[2*i+1] = char(lo);            
         }
         return string(s);
     }
 
-    function char(bytes1 b) internal pure returns (bytes1 c) {
-        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
-        else return bytes1(uint8(b) + 0x57);
+    function char(uint8 b) internal pure returns (bytes1 c) {
+        if (b < 10) return bytes1(b + 0x30);
+        else return bytes1(b + 0x57);
     }
 
     function hashLastChar(uint n) external view returns(string memory){
