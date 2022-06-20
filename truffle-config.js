fef8869f7a38748e5e63fce30e644b3f4b429a60
coinsccg <59504432+coinsccg@users.noreply.github.com>
2022-06-20 10:28:17 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -24,24 +24,24 @@ module.exports = {
   },
   networks: {
     rinkeby: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/457c1ac43c544b05abfef0163084a7a6`),
+      provider: () => new HDWalletProvider(process.env.PRIVKEY, `https://rinkeby.infura.io/v3/457c1ac43c544b05abfef0163084a7a6`),
       network_id: 4,
       skipDryRun: true
     },
     bscTest: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(process.env.PRIVKEY, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       timeoutBlocks: 200,
       skipDryRun: true
     },
     bsc: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed1.binance.org`),
+      provider: () => new HDWalletProvider(process.env.PRIVKEY, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
       timeoutBlocks: 200,
       skipDryRun: true
     },
     hecoTest: {//验证失败
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://http-testnet.hecochain.com/`),
+      provider: () => new HDWalletProvider(process.env.PRIVKEY, `https://http-testnet.hecochain.com/`),
       network_id: 256,
       timeoutBlocks: 200,
       skipDryRun: true
