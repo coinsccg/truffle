5a5d5289601961e00e0d0456bced54f928ab6ac2
coinsccg <59504432+coinsccg@users.noreply.github.com>
2022-08-24 16:59:15 +0800

    Update encode_and_decode_abi.js

--- script/encode_and_decode_abi.js
+++ script/encode_and_decode_abi.js
@@ -1,31 +1,24 @@
-var Web3 = require('web3');
+var web3 = require('web3');
 
-var web3 = new Web3();
+var w3 = new web3();
 
-var abi = web3.eth.abi.encodeFunctionCall({
-    name: 'myMethod',
+var funcName = "myMethod";
+var inputType = [{
+    type: 'uint256',
+    name: 'myNumber'
+},{
+    type: 'string',
+    name: 'myString'
+}];
+var inputParam = ['2345675643', 'Hello!%'];
+var funcCallInputBytes = w3.eth.abi.encodeFunctionCall({
+    name: funcName,
     type: 'function',
-    inputs: [{
-        type: 'uint256',
-        name: 'myNumber'
-    },{
-        type: 'string',
-        name: 'myString'
-    }]
-}, ['2345675643', 'Hello!%']);
-console.log(abi)
+    inputs: inputType
+}, inputParam);
+console.log(funcCallInputBytes)
 
+// var funcCallInputBytes = "0x24ee0097000000000000000000000000000000000000000000000000000000008bd02b7b0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000748656c6c6f212500000000000000000000000000000000000000000000000000"
 
-
-var abiHex = "0x24ee0097000000000000000000000000000000000000000000000000000000008bd02b7b0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000748656c6c6f212500000000000000000000000000000000000000000000000000"
-
-var param = web3.eth.abi.decodeParameters([
-    {
-        type: 'uint256',
-        name: 'myNumber'
-    },{
-        type: 'string',
-        name: 'myString'
-    }
-  ], "0x" + abiHex.slice(10))
-  console.log(param)
+var param = w3.eth.abi.decodeParameters(inputType, "0x" + funcCallInputBytes.slice(10))
+console.log(param["myNumber"], param["myString"])
